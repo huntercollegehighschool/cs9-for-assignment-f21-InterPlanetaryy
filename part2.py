10abae65eb59ea3b919da3f021917db441d8590d
Arbion Haxhillari <93940229+InterPlanetaryy@users.noreply.github.com>
2021-12-26 01:57:17 +0000

    completed all parts

--- part2.py
+++ part2.py
@@ -10,4 +10,9 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 #write your code here
 n = int(input("How many positive cubes do you want to calculate the sum of? "))
 
-for i in range()
+total = 0
+
+for i in range(1, n + 1):
+  total = total + i**3
+
+print(total)
